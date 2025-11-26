fe056f4d234edbd618b0e869a5450aa45ba124e2
mcmadsv <madsdamv@outlook.com>
2025-11-26 13:36:58 +0100

    Initial commit

---
+++ src/main/java/dk/ek/adtoolbackend/AdToolBackendApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.adtoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdToolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdToolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/ek/adtoolbackend/AdToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.adtoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdToolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
