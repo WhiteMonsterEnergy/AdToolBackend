18a4a313be25c8be0fc659faca0eef00f9bd512d
Lucia <asqv0001@stud.ek.dk>
2025-12-16 10:53:09 +0100

    bug fix

--- src/main/java/dk/ek/adtoolbackend/security/SecurityConfig.java
+++ src/main/java/dk/ek/adtoolbackend/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.http.HttpMethod;
+
 
 @Configuration
 public class SecurityConfig {
@@ -19,6 +21,7 @@ public class SecurityConfig {
         http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/profile/register", "/api/profile/login").permitAll()
                 .anyRequest().authenticated()
         );
